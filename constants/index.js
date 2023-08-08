d0cb594c6d2e40d9028eb58524a206b3f357633c
qawiize <141689242+qawiize@users.noreply.github.com>
2023-08-08 03:35:30 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xbe4196a6ff7aab2a4cccc4b46f848b0414babb0b";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
